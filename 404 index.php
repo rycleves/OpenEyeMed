37c6307a220e29083d1042035461428a6e16200f
Ryan Cleveland <54086390+rycleves@users.noreply.github.com>
2023-01-16 14:27:22 -0600

    Create 404 index.php

---
+++ 404 index.php
@@ -0,0 +1,17 @@
+?>
+<div <?php echo _attr( '404_page', array( 'class' => 'ast-404-layout-1' ) ); ?> >
+
+	<?php astra_the_title( '<header class="page-header"><h1 class="page-title">', '</h1></header><!-- .page-header -->' ); ?>
+
+	<div class="page-content">
+
+		<<?php echo esc_attr( $astra_404_subtitle_tag ); ?> class="page-sub-title">
+			<?php echo esc_html( astra_default_strings( 'string-404-sub-title', false ) ); ?>
+		</<?php echo esc_attr( $astra_404_subtitle_tag ); ?>>
+
+		<div class="ast-404-search">
+			<?php the_widget( 'WP_Widget_Search' ); ?>
+		</div>
+
+	</div><!-- .page-content -->
+</div>
